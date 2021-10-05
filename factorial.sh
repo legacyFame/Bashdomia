2a12c52670c9896767fef879fb341393bb72c5eb
Hiro <laciferin@gmail.com>
2021-10-05 10:53:46 +0530

    Rest

--- factorial.sh
+++ factorial.sh
@@ -1,18 +1,22 @@
-fact(){
-   num = $1
-   if (($num<=0))
-   then 
-      return 1
-   else
-      return fact $num-1
-}
+#fact(){
+#   num = $1
+#   if (($num<=0))
+#   then
+#      return 1
+#   else
+#      return fact $num-1
+#  fi
+#}
+#
+#fact 10
+#Will not work .. Remember echo works better than return
 
 
 fact(){
   num=$1
   if (($num<=1)) 
   then 
-      echo 1 #tried return and $# to access didnt work
+      echo 1 #tried return and $# to access didnt work Echo works like return
   else
       x=$(fact $((num-1))) # Piped o/p to x. 
       echo $((num*x)) 
--- main.sh
+++ main.sh
@@ -1,27 +1,46 @@
-fact(){
-   num = $1
-   if (($num<=0))
-   then 
-      return 1
-   else
-      return fact $num-1
-}
+#!/bin/bash
 
+let "hi=3+3"
+echo $hi
+
+
+if ((1 > 1))
+then
+   echo $((1==0))
+elif((1<1))
+then
+   echo Elif
+else
+    echo Greater Than
+fi
+
+while ((1<1))
+ do
+  echo "hi"
+done
+
+for val in {1..5}
+do echo $val
+done
+
+
+fact (){
+  if (($1 <=0))
+then
+   return $1
+   fi
+}
 
 fact(){
   num=$1
-  if (($num<=1)) 
-  then 
+  if (($num<=1))
+  then
       echo 1 #tried return and $# to access didnt work
   else
-      x=$(fact $((num-1))) # Piped o/p to x. 
-      echo $((num*x)) 
+      x=$(fact $((num-1))) # Piped o/p to x.
+      echo $((num*x))
   fi
-}
+
+  }
 
 fact 4
-# echo $#
-
-
-echo fact 5
-
